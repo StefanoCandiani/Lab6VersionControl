1dd3978f504795be179cbbb157ef483d36fce185
stefanocandiani1 <scandiani22500@columbushs.com>
2023-10-25 14:06:20 -0400

    Decoder complete. Final push.

--- main.py
+++ main.py
@@ -9,7 +9,24 @@ def encode(input_string):
     output_string = ''.join(str(int(char) + 3) for char in input_string)
     return output_string
 
+def decode(input_string):
+    output_string = ''.join(str(int(char) - 3) for char in input_string)
+    return output_string
 
 def main():
-    if __name__ == '__main__':
-        main()
+    encoded_message = ''
+    while True:
+        print_menu()
+        option = input('\nPlease enter an option: ')
+        if option == '1':
+            message = input('Please enter a password to encode: ')
+            encoded_message = encode(message)
+            print('Your password has been encoded and stored!\n')
+        elif option == '2':
+            print(f'The encoded password is {encoded_message}, and the original password is {decode(encoded_message)}.\n')
+        elif option == '3':
+            break
+
+
+if __name__ == '__main__':
+    main()
